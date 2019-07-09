4e29c960d0b1bcbef7346d6a6f3a985deb357987
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-09 12:44:06 +0100

    Add functionality to get all posts to routeHandler

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,32 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const Posts = require('../data/db')
+const Posts = require("../data/db");
 
 // api/posts is the root
 
 // POST
 router.post("/", async (req, res) => {
   const { title, contents } = req.body;
-  if(title && contents) {
+  if (title && contents) {
     try {
-        const response = await Posts.insert({title, contents});
-        // add logic to find created post
-        res.status(201).json(response);
+      const response = await Posts.insert({ title, contents });
+      // add logic to find created post
+      res.status(201).json(response);
     } catch (err) {
-        res.status(500).json({errorMessage: "There was an error while saving the post to the database"})
+      res
+        .status(500)
+        .json({
+          errorMessage:
+            "There was an error while saving the post to the database"
+        });
     }
   } else {
-      res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
   }
 });
 
 router.post("/:id/comments", (req, res) => {
-  res.json("post api/posts/comments");
+  const { id } = req.params.id;
+  // do get logic first
 });
 
 // GET
-router.get("/", (req, res) => {
-  res.json("Get api/posts");
+router.get("/", async (req, res) => {
+  try {
+    const response = await Posts.find();
+    res.status(200).json(response);
+  } catch {
+    res
+      .status(500)
+      .json({ errorMessage: "The posts information could not be retrieved." });
+  }
 });
 
 router.get("/:id", (req, res) => {
@@ -39,12 +56,12 @@ router.get("/:id/comments", (req, res) => {
 
 // DELETE
 router.delete("/:id", (req, res) => {
-    res.json('delete api/posts/ by id')
+  res.json("delete api/posts/ by id");
 });
 
 // PUT
 router.put("/:id", (req, res) => {
-    res.json('update api/posts/ by id')
+  res.json("update api/posts/ by id");
 });
 
 module.exports = router;
