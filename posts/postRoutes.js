27bc6a95a18ca892a2a7499677d9abe24171aa82
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-09 12:28:43 +0100

    Add route handlers and test via postman

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,7 +1,38 @@
-const express = require("express"); // import
+const express = require("express");
 const router = express.Router();
+
+// api/posts is the root
+
+// POST
+router.post("/", (req, res) => {
+  res.json("post api/posts/");
+});
+
+router.post("/:id/comments", (req, res) => {
+  res.json("post api/posts/comments");
+});
+
+// GET
 router.get("/", (req, res) => {
-    res.send('Hello from api/users')
+  res.json("Get api/posts");
+});
+
+router.get("/:id", (req, res) => {
+  res.json("Get api/posts by id");
+});
+
+router.get("/:id/comments", (req, res) => {
+  res.json("Get api/posts/:id/comments");
+});
+
+// DELETE
+router.delete("/:id", (req, res) => {
+    res.json('delete api/posts/ by id')
+});
+
+// PUT
+router.put("/:id", (req, res) => {
+    res.json('update api/posts/ by id')
 });
 
 module.exports = router;
